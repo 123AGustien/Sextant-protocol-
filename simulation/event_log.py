829b9cc93904e4790403b199028f3e6748641d87
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-04-28 02:18:19 +0700

    Update event_log.py

--- simulation/event_log.py
+++ simulation/event_log.py
@@ -1,1 +1,23 @@
 
+class EventLog:
+    """
+simulation/event_log.py
+    
+simul  """
+
+    def __init__(self):
+        self.events = []
+
+    def record(self, event_type, node_name, message, source=None):
+        self.events.append({
+            "type": event_type,
+            "node": node_name,
+            "message": message,
+            "source": source
+        })
+
+    def get_all(self):
+        return self.events
+
+    def clear(self):
+        self.events = []
